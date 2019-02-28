204e13fd1e4e6f0cafcc115165d83a7d1c2fd9a9
Joshua Anderson <joshua.frederick.anderson@gmail.com>
2019-02-28 08:30:45 -0800

    wip

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,23 +5,28 @@ class HashClass
   end
 
   def []=(key, value)
+
   end
 
 
   def [](key)
+
   end
 
   def resize
+
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+
   end
 
   # Simple method to return the number of items in the hash
   def size
+
   end
 
 end
