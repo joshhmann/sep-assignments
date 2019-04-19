4b84e3f001db6aca5ef0edff256cce9fb7f0de63
Josh Anderson <joshua.frederick.anderson@gmail.com>
2019-04-18 23:52:12 -0700

    trees WIP

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -7,20 +7,62 @@ class BinarySearchTree
   end
 
   def insert(root, node)
-
+    if root.rating > node.rating
+      if root.left.nil?
+      root.left = node
+      else
+      insert(root.left, node)
+      end
+    else
+      if root.right.nil?
+        root.right = node
+      else
+        insert(root.right, node)
+      end
+    end
   end
 
   # Recursive Depth First Search
   def find(root, data)
-
+    if root == nil || data == nil
+      return nil
+    else
+      if root.title == data
+        return root
+      elsif root.left != nil
+        find(root.left, data)
+      elsif root.right != nil
+        find(root.right, data)
+      end
+    end
   end
 
   def delete(root, data)
-
+    find_node = find(root, data)
+    if find_node == nil
+      return nil
+    else
+      find_node.title = nil
+      find_node.rating = nil
+    end
   end
 
   # Recursive Breadth First Search
   def printf(children=nil)
-
+    queue = [@root]
+    result = []
+    while queue.length > 0
+      new_root = queue.shift
+      if new_root.left != nil
+        queue.push(new_root.left)
+      end
+      if new_root.right != nil
+        queue.push(new_root.right)
+      end
+      result.push("#{new_root.title}: #{new_root.rating}")
+    end
+    result.each do |x|
+      puts x
+    end 
   end
 end
--- 01-data-structures/06-trees/min binary heap/node.rb
+++ 01-data-structures/06-trees/min binary heap/node.rb
@@ -1,13 +1,16 @@
 class Node
   attr_accessor :title
   attr_accessor :rating
-  attr_accessor :left
   attr_accessor :right
+  attr_accessor :left
+  attr_accessor :parent
 
   def initialize(title, rating)
     @title = title
     @rating = rating
-    @left = nil
-    @right = nil
   end
-end
+
+  def children
+    [@left, @right].compact
+  end
+end 
